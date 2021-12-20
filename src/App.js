e437252b775a9978650c50841ea2619c23e3ee4a
Kelvin <kelvinjune2@chaka.ng>
2021-12-20 23:21:33 +0100

    added redux toolkit and worked with slices

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Fragment } from 'react';
 import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import { useSelector } from 'react-redux';
 
 function App() {
-  return <Counter />;
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
+  return (
+    <Fragment>
+      <Header />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
+      <Counter />;
+    </Fragment>
+  );
 }
 
 export default App;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,38 @@
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { counterActions } from '../store/counter';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const { counter, showCounter } = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(20));
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={increaseHandler}>Increase By 20</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
